a9e32a3f3fc393037786559c45124648590cf519
Wang Jiezhe <wangjiezhe@users.noreply.github.com>
2016-02-19 18:02:18 +0800

    Simplify

--- ipgw.sh
+++ ipgw.sh
@@ -1,39 +1,39 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -f ~/.pkuipgwrc ]
+if [ -f ~/.ipgwrc ]
 then
-	source ~/.pkuipgwrc
-elif [ -f /etc/pkuipgwrc ]
+	source ~/.ipgwrc
+elif [ -f /etc/ipgwrc ]
 then
-	source /etc/pkuipgwrc
+	source /etc/ipgwrc
 fi
 
 [[ -z "$username" ]] && read -p "Please enter your username: " username
 [[ -z "$password" ]] && read -s -p "Please enter your password: " password
 
-URL="https://its.pku.edu.cn:5428/ipgatewayofpku?timeout=1"
+URL="https://its.pku.edu.cn:5428/ipgatewayofpku?timeout=1&uid=${username}&password=${password}"
 
 connect() {
-	RESULT=$(w3m -dump "${URL}&uid=${username}&password=${password}&range=2&operation=connect" 2>/dev/null | uniq)
+	RESULT=$(w3m -dump "${URL}&range=2&operation=connect" 2>/dev/null | uniq)
 	echo "$RESULT"
 	echo "$RESULT"|grep "Connect successfully" >/dev/null
 }
 
 disconnect() {
-	RESULT=$(w3m -dump "${URL}&uid=${username}&password=${password}&range=2&operation=disconnect" 2>/dev/null | uniq)
+	RESULT=$(w3m -dump "${URL}&range=2&operation=disconnect" 2>/dev/null | uniq)
 	echo "$RESULT"
 	echo "$RESULT"|grep "Disconnect Succeeded" >/dev/null
 }
 
 connect_global() {
-	RESULT=$(w3m -dump "${URL}&uid=${username}&password=${password}&range=1&operation=connect" 2>/dev/null | uniq)
+	RESULT=$(w3m -dump "${URL}&range=1&operation=connect" 2>/dev/null | uniq)
 	echo "$RESULT"
 	echo "$RESULT"|grep "Connect successfully" >/dev/null
 }
 
 disconnect_all() {
-	RESULT=$(w3m -dump "${URL}&uid=${username}&password=${password}&range=2&operation=disconnectall" 2>/dev/null | uniq)
+	RESULT=$(w3m -dump "${URL}&range=2&operation=disconnectall" 2>/dev/null | uniq)
 	echo "$RESULT"
 	echo "$RESULT"|grep "Disconnect All Succeeded" >/dev/null
 }
@@ -47,14 +47,14 @@ error() {
 if [ $# -eq 0 ]
 then
 	connect && exit 0
-	disconnect_all || exit 1
+	disconnect || exit 1
 	connect || exit 1
 elif [ $# -eq 1 ]
 then
 	case $1 in
 		g)
 			connect_global && exit 0
-			disconnect_all || exit 1
+			disconnect || exit 1
 			connect_global || exit 1
 			;;
 		d)
@@ -64,6 +64,10 @@ then
 			disconnect_all || exit 1
 			;;
 		r)
+			disconnect || exit 1
+			connect || exit 1
+			;;
+		ra)
 			disconnect_all || exit 1
 			connect || exit 1
 			;;
