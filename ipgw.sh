057a95852c453886188063f8b84b02babcc6683d
Wang Jiezhe <wangjiezhe@users.noreply.github.com>
2017-02-09 18:47:04 +0800

    Use new gateway

--- ipgw.sh
+++ ipgw.sh
@@ -3,43 +3,43 @@ set -e
 
 if [ -f ~/.ipgwrc ]
 then
-	source ~/.ipgwrc
+	conf_file="~/.ipgwrc"
 elif [ -f /etc/ipgwrc ]
 then
-	source /etc/ipgwrc
+	conf_file="/etc/ipgwrc"
 fi
 
-[[ -z "$username" ]] && read -p "Please enter your username: " username
-[[ -z "$password" ]] && read -s -p "Please enter your password: " password
+if [ -n ${conf_file} ]
+then
+	source <(grep = ${conf_file} | tr -d ' ')
+fi
 
-URL="https://its.pku.edu.cn:5428/ipgatewayofpku?timeout=1&uid=${username}&password=${password}"
+[ -z ${username} ] && read -p "Please enter your username: " username
+[ -z ${password} ] && read -s -p "Please enter your password: " password
+
+cURL() {
+	curl -A "IPGWLinux1.0_Linux_00_00_de_ad_be_ef" -sS \
+		"https://its.pku.edu.cn/cas/ITSClient" -d "$@"
+}
 
 connect() {
-	RESULT=$(w3m -dump "${URL}&range=2&operation=connect" 2>/dev/null | uniq)
-	echo "$RESULT"
-	echo "$RESULT"|grep "Connect successfully" >/dev/null
+	cURL "cmd=open&username=$username&password=$password&iprange=free" | jq .
 }
 
 disconnect() {
-	RESULT=$(w3m -dump "${URL}&range=2&operation=disconnect" 2>/dev/null | uniq)
-	echo "$RESULT"
-	echo "$RESULT"|grep "Disconnect Succeeded" >/dev/null
+	cURL "cmd=close" | jq .
 }
 
 connect_global() {
-	RESULT=$(w3m -dump "${URL}&range=1&operation=connect" 2>/dev/null | uniq)
-	echo "$RESULT"
-	echo "$RESULT"|grep "Connect successfully" >/dev/null
+	cURL "cmd=open&username=$username&password=$password&iprange=fee" | jq .
 }
 
 disconnect_all() {
-	RESULT=$(w3m -dump "${URL}&range=2&operation=disconnectall" 2>/dev/null | uniq)
-	echo "$RESULT"
-	echo "$RESULT"|grep "Disconnect All Succeeded" >/dev/null
+	cURL "cmd=closeall&username=$username&password=$password" | jq .
 }
 
 error() {
-	echo "Usage: $(basename "$0") [g|d|da|r]"
+	echo "Usage: $(basename "$0") [g|d|da|r|ra]"
 	exit 2
 }
 
@@ -47,14 +47,14 @@ error() {
 if [ $# -eq 0 ]
 then
 	connect && exit 0
-	disconnect || exit 1
-	connect || exit 1
+	# disconnect_all || exit 1
+	# connect || exit 1
 elif [ $# -eq 1 ]
 then
 	case $1 in
 		g)
 			connect_global && exit 0
-			disconnect || exit 1
+			disconnect_all || exit 1
 			connect_global || exit 1
 			;;
 		d)
